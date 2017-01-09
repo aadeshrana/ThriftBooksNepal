1aa2f15315ad74a1403081e407d74ac8e0d46b4f
Gaurav Jayasawal <jgaurav6@gmail.com>
2017-01-09 21:33:45 +0530

    this is ne commit

--- app/src/main/java/com/example/thearbiter/thriftbooksnepal/Information/infotest.java
+++ app/src/main/java/com/example/thearbiter/thriftbooksnepal/Information/infotest.java
@@ -4,4 +4,34 @@ package com.example.thearbiter.thriftbooksnepal.Information;
  * Created by Aadesh Rana on 09-01-17.
  */
 public class infotest {
+
+    /////******//////////////
+
+   /* 1. SplashScreen
+    2. SignUp
+    3. Login
+    4. MainDrawerHome
+    5. AlevelOptions
+    6. PlusTwoOptions
+    7. IbOptions
+    8. AdapterAlevelBuy
+    9. IbAdapterBuy
+    10.PlusTwoAdapterBuy
+    11.ActivitySeller
+    12.FragmentAlevelBuy
+    13.FragmentPlusTwoBuy
+    14.FragmentIbBuy
+    15.InformationMenuItems
+    16.InfromationBuyerRecycler
+    17.FragmentNavMenu
+    18.AdapterNavMenu
+    19.ActivityFinalBuy
+    20.ActivityAccount
+    21.ActivityAboutUs
+    */
+
+    //////*****//////
+
+
 }
+
--- app/src/main/java/com/example/thearbiter/thriftbooksnepal/MainActivity.java
+++ app/src/main/java/com/example/thearbiter/thriftbooksnepal/MainActivity.java
@@ -38,32 +38,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /////******//////////////
-
-   /* 1. SplashScreen
-    2. SignUp
-    3. Login
-    4. MainDrawerHome
-    5. AlevelOptions
-    6. PlusTwoOptions
-    7. IbOptions
-    8. AdapterAlevelBuy
-    9. IbAdapterBuy
-    10.PlusTwoAdapterBuy
-    11.ActivitySeller
-    12.FragmentAlevelBuy
-    13.FragmentPlusTwoBuy
-    14.FragmentIbBuy
-    15.InformationMenuItems
-    16.InfromationBuyerRecycler
-    17.FragmentNavMenu
-    18.AdapterNavMenu
-    19.ActivityFinalBuy
-    20.ActivityAccount
-    21.ActivityAboutUs
-    */
-
-    //////*****//////
-
-
 }
