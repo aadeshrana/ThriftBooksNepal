0683704271d05710ae8ba3aad03319a6ec4a4c85
Aadesh Rana <aadesh_rana@hotmail.com>
2017-01-11 22:09:05 +0545

    Merged signup and drawer

--- app/src/main/java/com/example/thearbiter/thriftbooksnepal/Activities/IbOptions.java
+++ app/src/main/java/com/example/thearbiter/thriftbooksnepal/Activities/IbOptions.java
@@ -19,6 +19,7 @@ public class IbOptions extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
     @Override
