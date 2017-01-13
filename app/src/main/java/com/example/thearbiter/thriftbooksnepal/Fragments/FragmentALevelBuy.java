1ab6ca9a4dc6578e76780cf984cf1d8be0b6f2af
Gaurav Jayasawal <jgaurav6@gmail.com>
2017-01-13 21:32:53 +0530

    this is commiiitttted

--- app/src/main/java/com/example/thearbiter/thriftbooksnepal/Fragments/FragmentALevelBuy.java
+++ app/src/main/java/com/example/thearbiter/thriftbooksnepal/Fragments/FragmentALevelBuy.java
@@ -5,17 +5,24 @@ package com.example.thearbiter.thriftbooksnepal.Fragments;
  */
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.thearbiter.thriftbooksnepal.Adapters.ALevelAdapterBuy;
+import com.example.thearbiter.thriftbooksnepal.ExtraClasses.JSONParser;
 import com.example.thearbiter.thriftbooksnepal.Information.InformationBuyerRecycler;
 import com.example.thearbiter.thriftbooksnepal.R;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +32,10 @@ public class FragmentALevelBuy extends Fragment {
     public String price[] = {"3500", "3200", "850", "325", "3350", "1250","a","b","c","d","e","f"};
     public String sellerName[] = {"Gaurav", "Astha", "Bader", "Ramesh Prasad", "Hari Yadav", "Barsha Upadhyay","a","b","c","d","e","f"};
     public int img[] = {R.drawable.chemistrydemopicture, R.drawable.tomduncanphysicsdemopictures, R.drawable.mathsdemopictures, R.drawable.statisticsdemopictures, R.drawable.economicsdemopicture, R.drawable.generaldaperdemopicture,R.drawable.chemistrydemopicture,R.drawable.economicsdemopicture,R.drawable.generaldaperdemopicture,R.drawable.generaldaperdemopicture,R.drawable.generaldaperdemopicture,R.drawable.mathsdemopictures};
+
+    ArrayList<String> p = new ArrayList<>();
+    ArrayList<String> q = new ArrayList<>();
+    JSONParser jsonParser = new JSONParser();
 
     @Nullable
     @Override
@@ -50,6 +61,46 @@ public class FragmentALevelBuy extends Fragment {
         }
         return data;
         //
+    }
+
+    public class PullAllAlevelItems extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            try {
+
+                Log.d("Before Vie Orders", "");
+                List<NameValuePair> params1 = new ArrayList<>();
+
+                params1.add(new BasicNameValuePair("course", Login.nameofuser));
+                JSONObject json = jsonParser.makeHttpRequest(ORDERS_URL, "POST", params1);
+
+                p.clear();
+                q.clear();
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        p.add(json.getString("a" + i));
+                        q.add(json.getString("b" + i));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                VieOrdersFragment.title = new String[10];
+                VieOrdersFragment.description = new String[10];
+
+                VieOrdersFragment.title = p.toArray(new String[p.size()]);
+                VieOrdersFragment.description = q.toArray(new String[q.size()]);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
     }
 }
 
