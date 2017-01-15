04b0872e780f3a25b671dfaf3e2b679ea87882e4
Gaurav Jayasawal <jgaurav6@gmail.com>
2017-01-15 18:18:34 +0530

    THIS MUST ORKKK

--- app/src/main/java/com/example/thearbiter/thriftbooksnepal/Activities/ActivitySeller.java
+++ app/src/main/java/com/example/thearbiter/thriftbooksnepal/Activities/ActivitySeller.java
@@ -16,6 +16,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -663,6 +665,33 @@ public class ActivitySeller extends AppCompatActivity implements View.OnClickLis
             checkAllBoxes = false;
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_alevel_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
